8df68d1b784f99102e523c2f5f9f76ae7d61ac9b
harsha-aqfer <51314435+harsha-aqfer@users.noreply.github.com>
2023-01-28 15:53:34 +0530

    added get, update and delete todo apis

--- internal/db/db.go
+++ internal/db/db.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/harsha-aqfer/todo/pkg"
+	"strings"
+	"time"
 )
 
 type DB struct {
@@ -22,10 +24,10 @@ func NewDB(username, password, host, dbname string) (Store, error) {
 
 type Store interface {
 	ListTodos(all bool) ([]pkg.TodoResponse, error)
-	GetTodo() (*pkg.TodoResponse, error)
+	GetTodo(id int64) (*pkg.TodoResponse, error)
 	CreateTodo(tr *pkg.TodoRequest) error
-	UpdateTodo() error
-	DeleteTodo() error
+	UpdateTodo(id int64, tr *pkg.TodoRequest) error
+	DeleteTodo(id int64) error
 }
 
 func (db *DB) ListTodos(all bool) ([]pkg.TodoResponse, error) {
@@ -81,14 +83,70 @@ func (db *DB) CreateTodo(tr *pkg.TodoRequest) error {
 	return err
 }
 
-func (db *DB) GetTodo() (*pkg.TodoResponse, error) {
+func (db *DB) GetTodo(id int64) (*pkg.TodoResponse, error) {
+	query := "SELECT id, task, category, priority, created_at, completed_at FROM todo WHERE id = ?"
+
+	rows, err := db.SQL.Query(query, id)
+	if err != nil {
+		return nil, err
+	}
+
+	defer func() {
+		_ = rows.Close()
+	}()
+
+	for rows.Next() {
+		t := pkg.TodoResponse{}
+		var ct sql.NullTime
+
+		err = rows.Scan(&t.Id, &t.Task, &t.Category, &t.Priority, &t.CreatedAt, &ct)
+
+		if err != nil {
+			return nil, err
+		}
+		if ct.Valid {
+			t.CompletedAt = &ct.Time
+		}
+		return &t, nil
+	}
 	return nil, nil
 }
 
-func (db *DB) UpdateTodo() error {
-	return nil
+func (db *DB) UpdateTodo(id int64, tr *pkg.TodoRequest) error {
+	var (
+		qs     []string
+		params []interface{}
+	)
+
+	if tr.Task != "" {
+		qs = append(qs, "task = ?")
+		params = append(params, tr.Task)
+	}
+
+	if tr.Category != "" {
+		qs = append(qs, "category = ?")
+		params = append(params, tr.Category)
+	}
+
+	if tr.Priority != "" {
+		qs = append(qs, "priority = ?")
+		params = append(params, tr.Priority)
+	}
+
+	if tr.Done {
+		qs = append(qs, "done = ?")
+		params = append(params, int64(1))
+
+		qs = append(qs, "completed_at = ?")
+		params = append(params, time.Now().UTC())
+	}
+
+	params = append(params, id)
+	_, err := db.SQL.Exec(fmt.Sprintf("UPDATE todo SET %s WHERE id = ?", strings.Join(qs, ", ")), params...)
+	return err
 }
 
-func (db *DB) DeleteTodo() error {
-	return nil
+func (db *DB) DeleteTodo(id int64) error {
+	_, err := db.SQL.Exec("DELETE FROM todo WHERE id = ?", id)
+	return err
 }
--- pkg/types.go
+++ pkg/types.go
@@ -1,6 +1,10 @@
 package pkg
 
-import "time"
+import (
+	"fmt"
+	"strings"
+	"time"
+)
 
 type TodoRequest struct {
 	Task     string `json:"task"`
@@ -16,4 +20,26 @@ type TodoResponse struct {
 	Priority    string     `json:"priority"`
 	CreatedAt   *time.Time `json:"created_at"`
 	CompletedAt *time.Time `json:"completed_at,omitempty"`
+}
+
+func (tr *TodoRequest) Validate() error {
+	// task should be non-empty
+	if tr.Task == "" {
+		return fmt.Errorf("inadequate input parameters. Required field: task")
+	}
+
+	cat := tr.Category
+	tr.Category = strings.ToLower(tr.Category)
+
+	if tr.Category != "work" && tr.Category != "home" {
+		return fmt.Errorf("unknown category value: %s", cat)
+	}
+
+	pr := tr.Priority
+	tr.Priority = strings.ToLower(tr.Priority)
+
+	if tr.Priority != "low" && tr.Priority != "medium" && tr.Priority != "high" {
+		return fmt.Errorf("unknown priority value: %s", pr)
+	}
+	return nil
 }
